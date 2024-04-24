185a6aa92a46b2c71f90a4aed8ed8c3d972eae24
Ricardo Aviles <vazquez1546@outlook.es>
2024-04-23 19:58:07 -0700

    Codigo utilerias

--- CapaNegocio/Utilerias.cs
+++ CapaNegocio/Utilerias.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CapaNegocio
 {
-    internal class Utilerias
+    public static class Utilerias
     {
+        public static Boolean validaVacio(String valor)
+        {
+            if (valor.Length == 0)
+                return false;
+            else
+                return true;
+        }
     }
 }
