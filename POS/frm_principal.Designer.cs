f534a6abc5810eca373794efab6050a639847904
Mirk0312 <karimmirandadelosreyes@gmail.com>
2024-04-30 00:47:39 -0800

    boton configurado (arreglar cambios de ventanas)

--- POS/frm_principal.Designer.cs
+++ POS/frm_principal.Designer.cs
@@ -51,6 +51,12 @@
             progressBar1 = new ProgressBar();
             toolTip1 = new ToolTip(components);
             panel_principal = new Panel();
+            btn_operaciones = new Button();
+            btn_ventas = new Button();
+            btn_inventario = new Button();
+            btn_configuracion = new Button();
+            btn_usuario = new Button();
+            btn_cerrarSesion = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel2.SuspendLayout();
@@ -63,21 +69,19 @@
             panel1.BackColor = SystemColors.ActiveCaption;
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
-            panel1.Controls.Add(pictureBox1);
             panel1.Controls.Add(btn_cerrar);
-            panel1.Location = new Point(1, 1);
-            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Location = new Point(170, 1);
             panel1.Name = "panel1";
-            panel1.Size = new Size(955, 110);
+            panel1.Size = new Size(922, 147);
             panel1.TabIndex = 0;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 13.8F);
-            label2.Location = new Point(655, 69);
+            label2.Location = new Point(749, 92);
             label2.Name = "label2";
-            label2.Size = new Size(83, 25);
+            label2.Size = new Size(100, 31);
             label2.TabIndex = 3;
             label2.Text = "Permiso:";
             // 
@@ -85,18 +89,17 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 13.8F);
-            label1.Location = new Point(655, 10);
+            label1.Location = new Point(749, 13);
             label1.Name = "label1";
-            label1.Size = new Size(85, 25);
+            label1.Size = new Size(103, 31);
             label1.TabIndex = 2;
             label1.Text = "Nombre:";
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(20, 8);
-            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Location = new Point(3, 13);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(179, 92);
+            pictureBox1.Size = new Size(160, 123);
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             // 
@@ -104,73 +107,78 @@
             // 
             btn_cerrar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btn_cerrar.ForeColor = Color.Red;
-            btn_cerrar.Location = new Point(861, 10);
-            btn_cerrar.Margin = new Padding(3, 2, 3, 2);
+            btn_cerrar.Location = new Point(984, 13);
             btn_cerrar.Name = "btn_cerrar";
-            btn_cerrar.Size = new Size(82, 33);
+            btn_cerrar.Size = new Size(94, 44);
             btn_cerrar.TabIndex = 0;
             btn_cerrar.Text = "X";
             btn_cerrar.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
+            panel2.Controls.Add(btn_cerrarSesion);
+            panel2.Controls.Add(btn_usuario);
+            panel2.Controls.Add(btn_configuracion);
+            panel2.Controls.Add(btn_inventario);
+            panel2.Controls.Add(btn_ventas);
+            panel2.Controls.Add(btn_operaciones);
+            panel2.Controls.Add(btn_productos);
+            panel2.Controls.Add(btn_proveedor);
+            panel2.Controls.Add(pictureBox1);
             panel2.Controls.Add(btn_inicio);
             panel2.Controls.Add(btn_clientes);
             panel2.Controls.Add(btn_catalogos);
-            panel2.Controls.Add(btn_proveedor);
-            panel2.Controls.Add(btn_productos);
-            panel2.Location = new Point(1, 115);
-            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Location = new Point(1, 1);
             panel2.Name = "panel2";
-            panel2.Size = new Size(215, 358);
+            panel2.Size = new Size(171, 629);
             panel2.TabIndex = 1;
             // 
             // btn_inicio
             // 
-            btn_inicio.Location = new Point(28, 2);
-            btn_inicio.Margin = new Padding(3, 2, 3, 2);
+            btn_inicio.BackColor = SystemColors.ActiveCaption;
+            btn_inicio.Location = new Point(0, 152);
             btn_inicio.Name = "btn_inicio";
-            btn_inicio.Size = new Size(148, 64);
+            btn_inicio.Size = new Size(169, 33);
             btn_inicio.TabIndex = 8;
             btn_inicio.Text = "Inicio";
-            btn_inicio.UseVisualStyleBackColor = true;
+            btn_inicio.UseVisualStyleBackColor = false;
+            btn_inicio.Click += btn_inicio_Click;
             // 
             // btn_clientes
             // 
-            btn_clientes.Location = new Point(28, 72);
-            btn_clientes.Margin = new Padding(3, 2, 3, 2);
+            btn_clientes.Location = new Point(32, 230);
             btn_clientes.Name = "btn_clientes";
-            btn_clientes.Size = new Size(148, 64);
+            btn_clientes.Size = new Size(104, 33);
             btn_clientes.TabIndex = 7;
             btn_clientes.Text = "Clientes";
             btn_clientes.UseVisualStyleBackColor = true;
+            btn_clientes.Click += btn_clientes_Click;
             // 
             // btn_catalogos
             // 
-            btn_catalogos.Location = new Point(28, 140);
-            btn_catalogos.Margin = new Padding(3, 2, 3, 2);
+            btn_catalogos.BackColor = SystemColors.ActiveCaption;
+            btn_catalogos.Location = new Point(0, 191);
             btn_catalogos.Name = "btn_catalogos";
-            btn_catalogos.Size = new Size(148, 64);
+            btn_catalogos.Size = new Size(169, 33);
             btn_catalogos.TabIndex = 6;
             btn_catalogos.Text = "Catalogos";
-            btn_catalogos.UseVisualStyleBackColor = true;
+            btn_catalogos.UseVisualStyleBackColor = false;
             // 
             // btn_proveedor
             // 
-            btn_proveedor.Location = new Point(28, 208);
-            btn_proveedor.Margin = new Padding(3, 2, 3, 2);
+            btn_proveedor.Location = new Point(32, 269);
             btn_proveedor.Name = "btn_proveedor";
-            btn_proveedor.Size = new Size(148, 64);
+            btn_proveedor.Size = new Size(104, 33);
             btn_proveedor.TabIndex = 5;
             btn_proveedor.Text = "Proveedores";
             btn_proveedor.UseVisualStyleBackColor = true;
+            btn_proveedor.Click += btn_proveedor_Click;
             // 
             // btn_productos
             // 
-            btn_productos.Location = new Point(28, 281);
-            btn_productos.Margin = new Padding(3, 2, 3, 2);
+            btn_productos.Location = new Point(32, 308);
             btn_productos.Name = "btn_productos";
-            btn_productos.Size = new Size(148, 64);
+            btn_productos.Size = new Size(104, 33);
             btn_productos.TabIndex = 4;
             btn_productos.Text = "Productos";
             btn_productos.UseVisualStyleBackColor = true;
@@ -178,10 +186,9 @@
             // 
             // panel3
             // 
-            panel3.Location = new Point(221, 115);
-            panel3.Margin = new Padding(3, 2, 3, 2);
+            panel3.Location = new Point(253, 153);
             panel3.Name = "panel3";
-            panel3.Size = new Size(735, 358);
+            panel3.Size = new Size(840, 477);
             panel3.TabIndex = 2;
             // 
             // panel4
@@ -189,20 +196,20 @@
             panel4.BackColor = SystemColors.ActiveCaption;
             panel4.Controls.Add(toolStrip1);
             panel4.Dock = DockStyle.Bottom;
-            panel4.Location = new Point(0, 477);
-            panel4.Margin = new Padding(3, 2, 3, 2);
+            panel4.Location = new Point(0, 636);
             panel4.Name = "panel4";
-            panel4.Size = new Size(956, 95);
+            panel4.Size = new Size(1093, 127);
             panel4.TabIndex = 3;
             // 
             // toolStrip1
             // 
             toolStrip1.BackColor = Color.PaleTurquoise;
             toolStrip1.Dock = DockStyle.Bottom;
+            toolStrip1.ImageScalingSize = new Size(20, 20);
             toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripLabel1, toolStripLabel2, toolStripLabel3, toolStripLabel4, toolStripLabel5 });
-            toolStrip1.Location = new Point(0, 70);
+            toolStrip1.Location = new Point(0, 102);
             toolStrip1.Name = "toolStrip1";
-            toolStrip1.Size = new Size(956, 25);
+            toolStrip1.Size = new Size(1093, 25);
             toolStrip1.TabIndex = 0;
             toolStrip1.Text = "toolStrip1";
             // 
@@ -210,63 +217,119 @@
             // 
             toolStripLabel1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             toolStripLabel1.Name = "toolStripLabel1";
-            toolStripLabel1.Size = new Size(52, 22);
+            toolStripLabel1.Size = new Size(67, 22);
             toolStripLabel1.Text = "Usuario:";
             // 
             // toolStripLabel2
             // 
             toolStripLabel2.Name = "toolStripLabel2";
-            toolStripLabel2.Size = new Size(43, 22);
+            toolStripLabel2.Size = new Size(51, 22);
             toolStripLabel2.Text = "xxxxxx";
             // 
             // toolStripLabel3
             // 
             toolStripLabel3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             toolStripLabel3.Name = "toolStripLabel3";
-            toolStripLabel3.Size = new Size(42, 22);
+            toolStripLabel3.Size = new Size(53, 22);
             toolStripLabel3.Text = "Fecha:";
             // 
             // toolStripLabel4
             // 
             toolStripLabel4.Name = "toolStripLabel4";
-            toolStripLabel4.Size = new Size(83, 22);
+            toolStripLabel4.Size = new Size(101, 22);
             toolStripLabel4.Text = "DD/MM/YYYY";
             // 
             // toolStripLabel5
             // 
             toolStripLabel5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             toolStripLabel5.Name = "toolStripLabel5";
-            toolStripLabel5.Size = new Size(51, 22);
+            toolStripLabel5.Size = new Size(65, 22);
             toolStripLabel5.Text = "Version:";
             // 
             // progressBar1
             // 
-            progressBar1.Location = new Point(1, 547);
-            progressBar1.Margin = new Padding(3, 2, 3, 2);
+            progressBar1.Location = new Point(1, 729);
             progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(946, 22);
+            progressBar1.Size = new Size(1081, 29);
             progressBar1.TabIndex = 4;
             // 
             // panel_principal
             // 
-            panel_principal.Location = new Point(221, 115);
+            panel_principal.Location = new Point(170, 153);
+            panel_principal.Margin = new Padding(3, 4, 3, 4);
             panel_principal.Name = "panel_principal";
-            panel_principal.Size = new Size(735, 358);
+            panel_principal.Size = new Size(923, 477);
             panel_principal.TabIndex = 0;
+            // 
+            // btn_operaciones
+            // 
+            btn_operaciones.BackColor = SystemColors.ActiveCaption;
+            btn_operaciones.Location = new Point(0, 347);
+            btn_operaciones.Name = "btn_operaciones";
+            btn_operaciones.Size = new Size(169, 33);
+            btn_operaciones.TabIndex = 9;
+            btn_operaciones.Text = "Operaciones";
+            btn_operaciones.UseVisualStyleBackColor = false;
+            // 
+            // btn_ventas
+            // 
+            btn_ventas.Location = new Point(32, 386);
+            btn_ventas.Name = "btn_ventas";
+            btn_ventas.Size = new Size(104, 33);
+            btn_ventas.TabIndex = 10;
+            btn_ventas.Text = "Ventas";
+            btn_ventas.UseVisualStyleBackColor = true;
+            // 
+            // btn_inventario
+            // 
+            btn_inventario.Location = new Point(32, 425);
+            btn_inventario.Name = "btn_inventario";
+            btn_inventario.Size = new Size(104, 33);
+            btn_inventario.TabIndex = 11;
+            btn_inventario.Text = "Inventario";
+            btn_inventario.UseVisualStyleBackColor = true;
+            // 
+            // btn_configuracion
+            // 
+            btn_configuracion.BackColor = SystemColors.ActiveCaption;
+            btn_configuracion.Location = new Point(3, 464);
+            btn_configuracion.Name = "btn_configuracion";
+            btn_configuracion.Size = new Size(169, 33);
+            btn_configuracion.TabIndex = 12;
+            btn_configuracion.Text = "Configuracion";
+            btn_configuracion.UseVisualStyleBackColor = false;
+            // 
+            // btn_usuario
+            // 
+            btn_usuario.Location = new Point(32, 503);
+            btn_usuario.Name = "btn_usuario";
+            btn_usuario.Size = new Size(104, 33);
+            btn_usuario.TabIndex = 13;
+            btn_usuario.Text = "Usuarios";
+            btn_usuario.UseVisualStyleBackColor = true;
+            // 
+            // btn_cerrarSesion
+            // 
+            btn_cerrarSesion.BackColor = SystemColors.ActiveCaption;
+            btn_cerrarSesion.Location = new Point(0, 542);
+            btn_cerrarSesion.Name = "btn_cerrarSesion";
+            btn_cerrarSesion.Size = new Size(169, 51);
+            btn_cerrarSesion.TabIndex = 14;
+            btn_cerrarSesion.Text = "Cerrar Sesion";
+            btn_cerrarSesion.UseVisualStyleBackColor = false;
             // 
             // frm_principal
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonHighlight;
-            ClientSize = new Size(956, 572);
+            ClientSize = new Size(1093, 763);
             Controls.Add(panel_principal);
             Controls.Add(progressBar1);
             Controls.Add(panel4);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "frm_principal";
             Load += frmprincipal_Load;
             panel1.ResumeLayout(false);
@@ -305,5 +368,11 @@
         private ToolStripLabel toolStripLabel4;
         private ToolStripLabel toolStripLabel5;
         private Panel panel_principal;
+        private Button btn_cerrarSesion;
+        private Button btn_usuario;
+        private Button btn_configuracion;
+        private Button btn_inventario;
+        private Button btn_ventas;
+        private Button btn_operaciones;
     }
 }
--- POS/frm_principal.cs
+++ POS/frm_principal.cs
@@ -17,11 +17,34 @@ namespace POS
 
         private void btn_productos_Click(object sender, EventArgs e)
         {
-            frm_productos frm = new frm_productos();
-            frm.TopLevel = false;
-            panel_principal.Controls.Add(frm);
-            frm.Dock = DockStyle.Fill;
-            frm.Show();
+            frm_productos frmproductos = new frm_productos();
+            frmproductos.TopLevel = false;
+            panel_principal.Controls.Add(frmproductos);
+            frmproductos.Dock = DockStyle.Fill;
+            frmproductos.Show();
+        }
+
+        private void btn_clientes_Click(object sender, EventArgs e)
+        {
+            frm_clientes frmclientes = new frm_clientes();
+            frmclientes.TopLevel = false;
+            panel_principal.Controls.Add(frmclientes);
+            frmclientes.Dock = DockStyle.Fill;
+            frmclientes.Show();
+        }
+
+        private void btn_inicio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_proveedor_Click(object sender, EventArgs e)
+        {
+            frm_proveedores frmproveedor = new frm_proveedores();
+            frmproveedor.TopLevel = false;
+            panel_principal.Controls.Add(frmproveedor);
+            frmproveedor.Dock = DockStyle.Fill;
+            frmproveedor.Show();
         }
     }
 }
