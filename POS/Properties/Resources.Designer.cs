f9b3f94f8429af9ef4e688b3031128511566a605
Mirk0312 <karimmirandadelosreyes@gmail.com>
2024-05-09 09:52:25 -0800

    Login (Terminado)

--- POS/Properties/Resources.Designer.cs
+++ POS/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace POS.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Entrar {
+            get {
+                object obj = ResourceManager.GetObject("Entrar", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap GuardarArchivo {
             get {
                 object obj = ResourceManager.GetObject("GuardarArchivo", resourceCulture);
@@ -166,6 +176,26 @@ namespace POS.Properties {
         internal static System.Drawing.Bitmap logo {
             get {
                 object obj = ResourceManager.GetObject("logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LogoCamisa {
+            get {
+                object obj = ResourceManager.GetObject("LogoCamisa", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LogoPantalon {
+            get {
+                object obj = ResourceManager.GetObject("LogoPantalon", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
--- POS/frm_login.Designer.cs
+++ POS/frm_login.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            pictureBox4 = new PictureBox();
-            pictureBox3 = new PictureBox();
             label3 = new Label();
             pictureBox2 = new PictureBox();
             pictureBox1 = new PictureBox();
@@ -39,18 +37,20 @@
             txt_usuario = new TextBox();
             label2 = new Label();
             label1 = new Label();
+            pictureBox4 = new PictureBox();
+            pictureBox3 = new PictureBox();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.BackColor = SystemColors.ActiveCaption;
-            panel1.Controls.Add(pictureBox4);
+            panel1.BackColor = Color.Lavender;
             panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox4);
             panel1.Controls.Add(label3);
             panel1.Controls.Add(pictureBox2);
             panel1.Controls.Add(pictureBox1);
@@ -67,31 +67,11 @@
             panel1.TabIndex = 0;
             panel1.Paint += panel1_Paint;
             // 
-            // pictureBox4
-            // 
-            pictureBox4.Image = Properties.Resources.icons8_pent_50;
-            pictureBox4.Location = new Point(690, 16);
-            pictureBox4.Margin = new Padding(3, 4, 3, 4);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(59, 67);
-            pictureBox4.TabIndex = 9;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Image = Properties.Resources.icons8_camisa_50;
-            pictureBox3.Location = new Point(14, 16);
-            pictureBox3.Margin = new Padding(3, 4, 3, 4);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(66, 67);
-            pictureBox3.TabIndex = 8;
-            pictureBox3.TabStop = false;
-            // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(234, 33);
+            label3.Location = new Point(234, 48);
             label3.Name = "label3";
             label3.Size = new Size(309, 41);
             label3.TabIndex = 7;
@@ -100,60 +80,20 @@
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.icons8_candado_30;
-            pictureBox2.Location = new Point(86, 253);
+            pictureBox2.Location = new Point(131, 231);
             pictureBox2.Margin = new Padding(3, 4, 3, 4);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(37, 45);
-            pictureBox2.TabIndex = 6;
-            pictureBox2.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Image = Properties.Resources.icons8_pent_50;
-            pictureBox4.Location = new Point(690, 16);
-            pictureBox4.Margin = new Padding(3, 4, 3, 4);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(59, 67);
-            pictureBox4.TabIndex = 9;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Image = Properties.Resources.icons8_camisa_50;
-            pictureBox3.Location = new Point(14, 16);
-            pictureBox3.Margin = new Padding(3, 4, 3, 4);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(66, 67);
-            pictureBox3.TabIndex = 8;
-            pictureBox3.TabStop = false;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(234, 33);
-            label3.Name = "label3";
-            label3.Size = new Size(309, 41);
-            label3.TabIndex = 7;
-            label3.Text = "--Acceso al sistema--";
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = Properties.Resources.icons8_candado_30;
-            pictureBox2.Location = new Point(86, 253);
-            pictureBox2.Margin = new Padding(3, 4, 3, 4);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(37, 45);
+            pictureBox2.Size = new Size(30, 30);
             pictureBox2.TabIndex = 6;
             pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.icons8_usuario_30;
-            pictureBox1.Location = new Point(86, 193);
+            pictureBox1.Location = new Point(131, 178);
             pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(40, 43);
+            pictureBox1.Size = new Size(30, 30);
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
             // 
@@ -161,53 +101,73 @@
             // 
             btn_ingresar.BackColor = Color.DarkGray;
             btn_ingresar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_ingresar.Location = new Point(585, 219);
+            btn_ingresar.Image = Properties.Resources.Entrar;
+            btn_ingresar.ImageAlign = ContentAlignment.MiddleLeft;
+            btn_ingresar.Location = new Point(311, 298);
             btn_ingresar.Margin = new Padding(3, 4, 3, 4);
             btn_ingresar.Name = "btn_ingresar";
-            btn_ingresar.Size = new Size(120, 67);
+            btn_ingresar.Size = new Size(132, 67);
             btn_ingresar.TabIndex = 4;
-            btn_ingresar.Text = "Ingresar";
+            btn_ingresar.Text = "       Ingresar";
+            btn_ingresar.TextAlign = ContentAlignment.MiddleRight;
             btn_ingresar.UseVisualStyleBackColor = false;
             btn_ingresar.Click += btn_ingresar_Click;
             // 
             // txt_password
             // 
-            txt_password.Location = new Point(270, 272);
+            txt_password.Location = new Point(337, 238);
             txt_password.Margin = new Padding(3, 4, 3, 4);
             txt_password.Name = "txt_password";
-
             txt_password.PasswordChar = '*';
-
-            txt_password.Size = new Size(254, 27);
+            txt_password.Size = new Size(282, 27);
             txt_password.TabIndex = 3;
+            txt_password.TextChanged += txt_password_TextChanged;
             // 
             // txt_usuario
             // 
-            txt_usuario.Location = new Point(270, 205);
+            txt_usuario.Location = new Point(337, 181);
             txt_usuario.Margin = new Padding(3, 4, 3, 4);
             txt_usuario.Name = "txt_usuario";
-            txt_usuario.Size = new Size(254, 27);
+            txt_usuario.Size = new Size(282, 27);
             txt_usuario.TabIndex = 2;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(128, 265);
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(167, 238);
             label2.Name = "label2";
-            label2.Size = new Size(150, 32);
+            label2.Size = new Size(164, 23);
             label2.TabIndex = 1;
-            label2.Text = "Contraseña:";
+            label2.Text = "Ingrese contraseña:";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(133, 203);
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(167, 180);
             label1.Name = "label1";
-            label1.Size = new Size(109, 32);
+            label1.Size = new Size(148, 25);
             label1.TabIndex = 0;
-            label1.Text = "Usuario:";
+            label1.Text = "Ingrese usuario:";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.LogoPantalon;
+            pictureBox4.Location = new Point(549, 30);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(70, 70);
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.LogoCamisa;
+            pictureBox3.Location = new Point(158, 30);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(70, 70);
+            pictureBox3.TabIndex = 10;
+            pictureBox3.TabStop = false;
             // 
             // frm_login
             // 
@@ -222,17 +182,15 @@
             MdiChildrenMinimizedAnchorBottom = false;
             MinimizeBox = false;
             Name = "frm_login";
-
             ShowIcon = false;
             StartPosition = FormStartPosition.CenterScreen;
-
             Text = "Form1";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ResumeLayout(false);
         }
 
@@ -246,8 +204,8 @@
         private Button btn_ingresar;
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox3;
         private Label label3;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
     }
 }
