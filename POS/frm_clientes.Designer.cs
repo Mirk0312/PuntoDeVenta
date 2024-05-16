f611c238a3508e5cff76e6cc68b5a3ad856a6da4
Mirk0312 <karimmirandadelosreyes@gmail.com>
2024-05-15 18:12:47 -0800

    ..

--- POS/frm_clientes.Designer.cs
+++ POS/frm_clientes.Designer.cs
@@ -320,23 +320,30 @@
 
         #endregion
         private Button btn_EliminarCliente;
+        private TextBox txt_RFCClientes;
         private Button btn_AgregarC;
         private TextBox txt_RFCC;
         private Label label11;
+        private TextBox txt_CorreoClientes;
         private TextBox txt_CorreoC;
         private TextBox txt_TelefonoClientes;
+        private TextBox txt_NombreClientes;
         private TextBox txt_NombreC;
         private TextBox txt_ClaveC;
         private TextBox txt_NumeroclienteC;
+        private TextBox txt_ClaveClientes;
+        private TextBox txt_NumeroDeCliente;
         private Label label10;
         private Label label9;
         private Label label8;
         private Label label7;
         private Label label6;
         private PictureBox pictureBox1;
+        private Button btn_ActualizarCliente;
         private Button btn_ActualizarC;
         private Button btn_EditarCliente;
         private Panel panel2;
+        private Button btn_CerrarCliente;
         private Button btn_CerrarProduc;
         private Label label1;
         private Panel panel1;
