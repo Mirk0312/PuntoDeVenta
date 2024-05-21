657acf6ef217e6151fe5768f614c1495f14414c2
Ricardo Aviles <vazquez1546@outlook.es>
2024-05-21 10:37:26 -0700

    Diseño cliente

--- POS/frm_clientes.Designer.cs
+++ POS/frm_clientes.Designer.cs
@@ -45,13 +45,18 @@
             btn_actualizar = new Button();
             btn_eliminar = new Button();
             button1 = new Button();
+            panel2 = new Panel();
+            label7 = new Label();
+            pictureBox1 = new PictureBox();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 13.8F);
-            label1.Location = new Point(24, 72);
+            label1.Location = new Point(33, 117);
             label1.Name = "label1";
             label1.Size = new Size(36, 31);
             label1.TabIndex = 0;
@@ -61,7 +66,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 13.8F);
-            label2.Location = new Point(26, 132);
+            label2.Location = new Point(35, 177);
             label2.Name = "label2";
             label2.Size = new Size(98, 31);
             label2.TabIndex = 1;
@@ -71,7 +76,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 13.8F);
-            label3.Location = new Point(26, 281);
+            label3.Location = new Point(35, 326);
             label3.Name = "label3";
             label3.Size = new Size(69, 31);
             label3.TabIndex = 2;
@@ -81,7 +86,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 13.8F);
-            label4.Location = new Point(24, 408);
+            label4.Location = new Point(33, 453);
             label4.Name = "label4";
             label4.Size = new Size(100, 31);
             label4.TabIndex = 3;
@@ -91,7 +96,7 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 13.8F);
-            label5.Location = new Point(26, 191);
+            label5.Location = new Point(35, 236);
             label5.Name = "label5";
             label5.Size = new Size(53, 31);
             label5.TabIndex = 4;
@@ -101,7 +106,7 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 13.8F);
-            label6.Location = new Point(26, 354);
+            label6.Location = new Point(35, 399);
             label6.Name = "label6";
             label6.Size = new Size(82, 31);
             label6.TabIndex = 5;
@@ -109,51 +114,51 @@
             // 
             // txt_idCliente
             // 
-            txt_idCliente.Location = new Point(253, 78);
+            txt_idCliente.Location = new Point(262, 123);
             txt_idCliente.Name = "txt_idCliente";
             txt_idCliente.Size = new Size(192, 27);
             txt_idCliente.TabIndex = 6;
             // 
             // txt_nombre
             // 
-            txt_nombre.Location = new Point(253, 138);
+            txt_nombre.Location = new Point(262, 183);
             txt_nombre.Name = "txt_nombre";
             txt_nombre.Size = new Size(192, 27);
             txt_nombre.TabIndex = 7;
             // 
             // txt_rfc
             // 
-            txt_rfc.Location = new Point(253, 207);
+            txt_rfc.Location = new Point(262, 252);
             txt_rfc.Name = "txt_rfc";
             txt_rfc.Size = new Size(192, 27);
             txt_rfc.TabIndex = 8;
             // 
             // txt_correo
             // 
-            txt_correo.Location = new Point(253, 354);
+            txt_correo.Location = new Point(262, 399);
             txt_correo.Name = "txt_correo";
             txt_correo.Size = new Size(192, 27);
             txt_correo.TabIndex = 9;
             // 
             // txt_telefono
             // 
-            txt_telefono.Location = new Point(253, 414);
+            txt_telefono.Location = new Point(262, 459);
             txt_telefono.Name = "txt_telefono";
             txt_telefono.Size = new Size(192, 27);
             txt_telefono.TabIndex = 10;
             // 
             // txt_clave
             // 
-            txt_clave.Location = new Point(253, 297);
+            txt_clave.Location = new Point(262, 342);
             txt_clave.Name = "txt_clave";
             txt_clave.Size = new Size(192, 27);
             txt_clave.TabIndex = 11;
             // 
             // btn_guardar
             // 
-            btn_guardar.Location = new Point(640, 73);
+            btn_guardar.Location = new Point(566, 129);
             btn_guardar.Name = "btn_guardar";
-            btn_guardar.Size = new Size(94, 29);
+            btn_guardar.Size = new Size(94, 42);
             btn_guardar.TabIndex = 12;
             btn_guardar.Text = "Guardar";
             btn_guardar.UseVisualStyleBackColor = true;
@@ -161,9 +166,9 @@
             // 
             // btn_nuevo
             // 
-            btn_nuevo.Location = new Point(640, 170);
+            btn_nuevo.Location = new Point(566, 226);
             btn_nuevo.Name = "btn_nuevo";
-            btn_nuevo.Size = new Size(94, 29);
+            btn_nuevo.Size = new Size(94, 42);
             btn_nuevo.TabIndex = 13;
             btn_nuevo.Text = "Nuevo";
             btn_nuevo.UseVisualStyleBackColor = true;
@@ -171,9 +176,9 @@
             // 
             // btn_actualizar
             // 
-            btn_actualizar.Location = new Point(640, 281);
+            btn_actualizar.Location = new Point(566, 337);
             btn_actualizar.Name = "btn_actualizar";
-            btn_actualizar.Size = new Size(94, 29);
+            btn_actualizar.Size = new Size(94, 42);
             btn_actualizar.TabIndex = 14;
             btn_actualizar.Text = "Actualizar";
             btn_actualizar.UseVisualStyleBackColor = true;
@@ -181,9 +186,9 @@
             // 
             // btn_eliminar
             // 
-            btn_eliminar.Location = new Point(640, 371);
+            btn_eliminar.Location = new Point(566, 427);
             btn_eliminar.Name = "btn_eliminar";
-            btn_eliminar.Size = new Size(94, 29);
+            btn_eliminar.Size = new Size(94, 42);
             btn_eliminar.TabIndex = 15;
             btn_eliminar.Text = "Eliminar";
             btn_eliminar.UseVisualStyleBackColor = true;
@@ -191,20 +196,54 @@
             // 
             // button1
             // 
-            button1.Location = new Point(742, 12);
+            button1.Location = new Point(754, 18);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(94, 36);
             button1.TabIndex = 16;
             button1.Text = "Cerrar";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DarkBlue;
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(860, 75);
+            panel2.TabIndex = 43;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.DarkBlue;
+            label7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.AliceBlue;
+            label7.Location = new Point(132, 23);
+            label7.Name = "label7";
+            label7.Size = new Size(99, 31);
+            label7.TabIndex = 21;
+            label7.Text = "Clientes";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.SlateGray;
+            pictureBox1.Image = Properties.Resources.icons8_clientes_100;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(98, 60);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
             // 
             // frm_clientes
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(860, 505);
-            Controls.Add(button1);
+            Controls.Add(panel2);
             Controls.Add(btn_eliminar);
             Controls.Add(btn_actualizar);
             Controls.Add(btn_nuevo);
@@ -225,6 +264,9 @@
             Name = "frm_clientes";
             Text = "frm_clientes";
             Load += frm_clientes_Load;
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -248,5 +290,8 @@
         private Button btn_actualizar;
         private Button btn_eliminar;
         private Button button1;
+        private Panel panel2;
+        private Label label7;
+        private PictureBox pictureBox1;
     }
 }
